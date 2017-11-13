260fc7b0f62400ca2091225554addab2bb6ec06e
Julian <julian.palmanshofer@gmail.com>
2017-11-13 19:48:19 +0100

    configured scripts for usage of sass files; basic setup of file structure; implemented guest navbar (responsive). Changes required

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import Navbar from './components/navbar/NavbarGuest'
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div className="wrapper">
+          <Navbar />
+          <div className="navbarIcon">
+            <a onClick={this.showSidebar}>&#9776;</a>
+          </div>
+
+        </div>
+      </Router>
+    )
   }
+
+  showSidebar() {
+    let display = document.getElementById('navbar').style.display
+    let map = {
+      block: 'none',
+      none: 'block',
+      "": 'block'
+    }
+    document.getElementById('navbar').style.display = map[display]
+  }
+
 }
 
 export default App;
